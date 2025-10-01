680001693f4a74167efa380b0b9359b55af1ab84
Avrami Aharonoff <60356360+avramiaharonoff@users.noreply.github.com>
2025-10-01 11:17:01 -0400

    Update 01-run_primary_transcripts.sh

--- scripts/phylogenetic analysis/01-run_primary_transcripts.sh
+++ scripts/phylogenetic analysis/01-run_primary_transcripts.sh
@@ -1,2 +1,9 @@
-# extract the longest transcript variant per gene
-for f in *fa* ; do python /scratch/aa7013/OrthoFinder/tools/primary_transcript.py $f ; done
+# make a directory with all of the fasta files you want to run
+
+# enter directory
+cd /path/to/directory
+
+# extract the longest transcript variant per gene with the primary_transcript.py script provided by OrthoFinder (version 2.5.4)
+for f in *fa* ; do python /path/to/primary_transcript.py $f ; done
+
+# output is a fasta file for each input. Run OrthoFinder on all outputs with the next script.
