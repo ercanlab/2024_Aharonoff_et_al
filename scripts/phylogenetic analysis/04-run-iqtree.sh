b608ebfa07cb9730865a76f5bdda4cf15b2cf355
Avrami Aharonoff <60356360+avramiaharonoff@users.noreply.github.com>
2025-10-01 11:29:26 -0400

    Update 04-run-iqtree.sh

--- scripts/phylogenetic analysis/04-run-iqtree.sh
+++ scripts/phylogenetic analysis/04-run-iqtree.sh
@@ -1,7 +1,14 @@
-msa=MSA-local_OG0001299
+msa=MSA-local_OG0001775 # multiple sequence alignment (MSA) file is the input
+proj=out_rhabditids_18/SDC-1 # run directory
 
-../iqtree-2.2.0-Linux/bin/iqtree2 -s ./${msa}.fasta \
-    --seqtype AA \
-    -m MFP \
-    -B 1000 \
+# enter run directory
+cd ./${proj}
+
+# run iqtree
+../../iqtree-2.2.0-Linux/bin/iqtree2 -s ./${msa}.fasta \
+    --seqtype AA \ # the sequences are amino acids
+    -m MFP \ # run ModelFinder and choose appropriate model (see iqtree documentation)
+    -B 1000 \ # the number of bootstrap replicates
     --prefix ${msa}
+
+echo done
