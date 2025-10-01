68e6120e1b7210216e42c0b5080a21918e4da876
Avrami Aharonoff <60356360+avramiaharonoff@users.noreply.github.com>
2025-10-01 11:19:17 -0400

    Update 02-run-orthofinder.sh

--- scripts/phylogenetic analysis/02-run-orthofinder.sh
+++ scripts/phylogenetic analysis/02-run-orthofinder.sh
@@ -1,1 +1,4 @@
-./orthofinder -f ./<path>/primary_transcripts
+# /path/to/primary_transcripts is the directory with the fasta files (one per species)
+
+# run OrthoFinder (version 2.5.4) on the primary transcripts
+./orthofinder -f /path/to/primary_transcripts
