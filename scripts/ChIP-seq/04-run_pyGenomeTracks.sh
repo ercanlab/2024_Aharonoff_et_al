97e78feb30fc1d31593c48507583254a585dee37
Avrami Aharonoff <60356360+avramiaharonoff@users.noreply.github.com>
2025-11-17 13:57:04 -0500

    Update 04-run_pyGenomeTracks.sh

--- scripts/ChIP-seq/04-run_pyGenomeTracks.sh
+++ scripts/ChIP-seq/04-run_pyGenomeTracks.sh
@@ -10,11 +10,11 @@
 #SBATCH --mail-type=END
 #SBATCH --mail-user=aa7013@nyu.edu
 
-base_dir=/scratch/aa7013
-working_dir=ChIP_EVO2024_CEW1
-reg="X:6,656,756-6,717,416"
-chrom=X
-outName=H4K20me1_H3K4me3_IgG_all-reps_subset_CEW1_L-24hrs_oscheius_tipulae.PRJNA644888.WBPS16.genomic_inputsubtract
+base_dir=/scratch/aa7013 # base directory
+working_dir=ChIP_EVO2024_CEW1 # working directory
+reg="X:6,656,756-6,717,416" # region to plot
+chrom=X # chromosome
+outName=H4K20me1_H3K4me3_IgG_all-reps_subset_CEW1_L-24hrs_oscheius_tipulae.PRJNA644888.WBPS16.genomic_inputsubtract # out file name
 
 mkdir -p tracks
 
